3f01853858b3f1ebfb99869e807a1d54be24d72e
Maazkhan171210 <mehroozmnzr@gmail.com>
2026-03-26 15:48:32 +0500

    Update tooltip.tsx

--- artifacts/shah-mobile/src/App.tsx
+++ artifacts/shah-mobile/src/App.tsx
@@ -1,7 +1,6 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 
 import Home from "@/pages/home";
 import Store from "@/pages/store";
